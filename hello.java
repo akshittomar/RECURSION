569295983f4f466bcf14d8f0482ad31ecd4d0800
akshittomar <105296394+akshittomar@users.noreply.github.com>
2024-02-19 20:31:01 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello
 {
     public static void main(String args[])
     {
-        System.out.println("JAI SHIV SHANKAR  JAI Siya ram");
+        System.out.println("JAI SHIV SHANKAR  JAI Siya Ram");
     }
 }
